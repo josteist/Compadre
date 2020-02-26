ab2552e8048985cd272322429962afd667c51550
josteist <josteinstarrfelt@gmail.com>
2020-02-26 09:36:50 +0100

    updated getinit

--- R/getInit.R
+++ R/getInit.R
@@ -1,7 +1,8 @@
 #' A function to generate sensible initial values for MCMC estimation.
 #'
 #' getInits generates initial values for a complex CMR model. Essentially the global parameters (mean rates) are estimated optimizing the probability density function, assuming all random effects are 0. Then each random effect is estimated independently, assuming all other random effects are 0. This is not proper optimization, but work reasonably well for initial values.
-#' @param cmrModel A CMR model structure.
+#'
+#' @param cmrModel A CMR model structure with 1 clade.
 #' @return The function returns relatively decent initial values for MCMC estimation.
 #' @export
 #' @examples mod1 <- make.BayesCMR(Obs)
@@ -10,28 +11,50 @@
 
 
 
-# Getting initial values?
 
 getInit <- function(cmrModel){
   # THis function will generate probable initial values for the MCMC chain.
   # The main parameters will be estimated by optim ignoring the random effects afterwhich all the RE will be optimzed independently (1 dimension at a time).
-  # This will not be proper optimization, but be better initial values for analysis.
   # Change to also include drivers, but NOT variances.
-  if (cmrModel$npar>3){
-    x <- c(optim(c(-1,-1.1,-1.2),function(x){-cmrModel$probfun(c(x[1],x[2],x[3],rep(0,cmrModel$npar-3)))})$par,
-           rep(1,cmrModel$nhps-3),rep(0,cmrModel$npar-cmrModel$nhps))
-    xres <- sapply((min(unlist(cmrModel$reix))):(max(unlist(cmrModel$reix))),function(jj){
-      optim(0,function(yy){-cmrModel$probfun(c(x[1:(jj-1)],yy,x[(jj+1):cmrModel$npar]))},
-            method='Brent',lower=-10,upper=10)$par})
-    x[(cmrModel$nhps+1):cmrModel$npar]=xres;
+  if (length(cmrModel$inx)==2){
+    # it's a 2-clade omdel
+    x_1 <- c(optim(c(-1,-1.1,-1.2),function(x){-cmrModel$Clade1Mod$probfun(c(x[1],x[2],x[3],rep(0,cmrModel$Clade1Mod$npar-3)))})$par,
+             rep(0,cmrModel$Clade1Mod$npar-3))
+    xix <- sample(unlist(cmrModel$Clade1Mod$inx[5:7]))
+    if (length(xix)>0){
+      xres <- sapply(xix,function(jj){
+        optim(0,function(yy){-cmrModel$Clade1Mod$probfun(c(x_1[1:(jj-1)],yy,x_1[(jj+1):cmrModel$Clade1Mod$npar]))},
+              method='Brent',lower=-10,upper=10)$par})
+      x_1[xix]=xres;
+    }
+
+    x_2 <- c(optim(c(-1,-1.1,-1.2),function(x){-cmrModel$Clade2Mod$probfun(c(x[1],x[2],x[3],rep(0,cmrModel$Clade2Mod$npar-3)))})$par,
+             rep(0,cmrModel$Clade2Mod$npar-3))
+    xix <- sample(unlist(cmrModel$Clade2Mod$inx[5:7]))
+    if (length(xix)>0){
+      xres <- sapply(xix,function(jj){
+        optim(0,function(yy){-cmrModel$Clade2Mod$probfun(c(x_2[1:(jj-1)],yy,x_2[(jj+1):cmrModel$Clade2Mod$npar]))},
+              method='Brent',lower=-10,upper=10)$par})
+      x_2[xix]=xres;
+    }
+    x = c(x_1,x_2);
+
+
   } else {
-    x <- optim(c(-1,-1.1,-1.2),function(x){-cmrModel$probfun(x)})$par
+
+    if (cmrModel$npar>3){
+      x <- c(optim(c(-1,-1.1,-1.2),function(x){-cmrModel$probfun(c(x[1],x[2],x[3],rep(0,cmrModel$npar-3)))})$par,
+             rep(0,cmrModel$npar-3))
+      xix <- sample(unlist(cmrModel$inx[5:7]))
+      if (length(xix)>0){
+        xres <- sapply(xix,function(jj){
+          optim(0,function(yy){-cmrModel$probfun(c(x[1:(jj-1)],yy,x[(jj+1):cmrModel$npar]))},
+                method='Brent',lower=-10,upper=10)$par})
+        x[xix]=xres;
+      }
+    } else {
+      x <- optim(c(-1,-1.1,-1.2),function(x){-cmrModel$probfun(x)})$par
+    }
   }
   return(x0=x)
 }
-
-#
-# x <- fx1b$Chain[1,]
-# x2 <-   sapply(7:mx1$npar,
-#                function(jj){optim(c(-1),function(y){-mx1$probfun(c(x[1:jj],y,x[(jj+1):mx1$npar]))},method='Brent',lower=-10,upper=10)$par})
-#
