a184d404e086792c42afd12daf62290de1c051af
josteist <josteinstarrfelt@gmail.com>
2020-02-28 08:22:10 +0100

    fixed a bug in pmin

--- R/mypmin.R
+++ R/mypmin.R
@@ -1,10 +1,10 @@
 #' @export
 # quick function to check for too probabilities too close to 0 or 1 inside pradel_unvd_gam
 mypmin <- function(x,minval){
-  if (sum(x<minval)){
+  if (any(x<minval)){
     x[which(x<minval)] = minval;
   }
-  if (sum(x>(1-minval))){
+  if (any(x>(1-minval))){
     x[which(x>(1-minval))] = 1-minval
   }
 
